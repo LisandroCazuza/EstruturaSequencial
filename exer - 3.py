cf467698199f516559ee245805ca2ff0edd80104
LisandroCazuza <geohist2005@gmail.com>
2022-04-17 15:57:10 -0300

    EstruturaSequencial

--- exer - 3.py
+++ exer - 3.py
@@ -1,3 +1,5 @@
 """3.Faça um Programa que peça dois números e imprima a soma."""
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
+soma = n1 + n2
+print(f"A soma é: {soma}")
